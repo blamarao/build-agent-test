217fcd019d402c338d9d599eb446d68e0ef9ef79
Bruno Clementino Lamarao <Bruno.Lamarao@coolblue.nl>
2018-08-20 13:51:44 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace build_agent_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GlobalPosition position = new GlobalPosition(51.9233076,4.4794524);
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(position.PrintMe());
+        }
+
+        public class GlobalPosition
+        {
+            private (double, double) _coordinates;
+
+            public GlobalPosition(double latitude, double longitude)
+            {
+                _coordinates = (latitude, longitude);
+            }
+
+            public string PrintMe() => $"Your current position is {_coordinates.Item1},{_coordinates.Item2}";
+        }
+    }
+}
